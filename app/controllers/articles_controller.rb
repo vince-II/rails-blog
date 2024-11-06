cc6c50ec89ca52ef63ca039b0f15c0feadf95127
vince-II <arnievincent.nery@gmail.com>
2024-11-06 17:59:10 +0800

    article_path helper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
-require "application_controller" # DON'T DO THIS.
+# require "application_controller" # DON'T DO THIS.
 
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def show
+    # params <-- coming from endpoint articles/id
+    @articles = Article.find(params[:id])
   end
 end
